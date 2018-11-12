d3e8c2b66aa82120a3217ad68b58f89bcdda2693
skyForge <1281alexdeo@gmail.com>
2018-11-12 15:49:22 +1300

    replace Learn more to DEMO

--- client/src/Components/Project/ProjectItem.js
+++ client/src/Components/Project/ProjectItem.js
@@ -5,11 +5,11 @@ export const ProjectItem = ({ project }) => {
       key={project.id}
       className="col-md-6 col-sm-12 col-lg-4  project-item text-center"
     >
-      <img className="project_image" src={project.img} alt="projectPic" />
+      <img className="project_image" src={project.img} alt="" />
       <h2 className="mb-2">{project.title}</h2>
       <h5 className="mb-4">{project.tech}</h5>
       <a className="btn btn-outline-teal" href={project.url} target="_blank">
-        LEARN MORE
+        DEMO
       </a>
     </div>
   );
