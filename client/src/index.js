4a9d81b51fc331b7b554deb5bf2fefee8a537082
skyForge <1281alexdeo@gmail.com>
2019-01-16 13:00:19 +1200

    updated my resume

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { registerObserver } from 'react-perf-devtool';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
+registerObserver();
 serviceWorker.unregister();
